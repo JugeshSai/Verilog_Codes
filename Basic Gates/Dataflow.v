f3be6d9d786eac342cebe006cdc5ab416e23bc9f
JugeshSai <141944621+JugeshSai@users.noreply.github.com>
2025-05-27 15:21:48 +0530

    Create Dataflow.v

---
+++ Basic Gates/Dataflow.v
@@ -0,0 +1,13 @@
+module And(
+  input a,b,
+  output out_and,out_or,out_not,out_nand,out_nor,out_xor,out_xnor
+);
+  assign out_and=a&b;
+  assign out_or=a|b;
+  assign out_not=~a;
+  assign out_nand=~(a&b);
+  assign out_nor=~(a|b);
+  assign out_xor=a^b;
+  assign out_xnor=~(a^b);
+  
+endmodule
