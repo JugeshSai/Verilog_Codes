c06d4b06bb0f26f9124b41a35f0236bd3539cd53
JugeshSai <141944621+JugeshSai@users.noreply.github.com>
2025-05-27 15:57:52 +0530

    Create Basic_Gates_tb.v

---
+++ Basic Gates/Basic_Gates_tb.v
@@ -0,0 +1,19 @@
+module Basic_Gates_tb;
+  wire out_and,out_or,out_not,out_nand,out_nor,out_xor,out_xnor;
+  reg a,b;
+  dataflow dut(.a(a),.b(b),.out_and(out_and),.out_or(out_or),.out_not(out_not),out_nand(out_nand),
+               out_nor(out_nor),out_xor(out_xor),out_xnor(out_xnor));
+  initial
+    begin
+      a=1'b0; b=1'b0;
+      #10 a=1'b0; b=1'b1;
+      #10 a=1'b1; b=1'b0;
+      #10 a=1'b1; b=1'b1;
+      #50 $finish;
+    end
+  always @(*)
+    begin
+      $display(" Inputs: a=%b b=%b Outputs: AND=%b OR=%b NOT=%b NAND=%b NOR=%B XOR=%b XNOR=%b" , a,b,out_and,out_or,
+               out_not,out_nand,out_nor,out_xor,out_xnor);
+    end
+endmodule
