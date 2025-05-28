e15f30796ab392c70e158e16b10e4333b5b655a3
JugeshSai <141944621+JugeshSai@users.noreply.github.com>
2025-05-28 14:42:31 +0530

    Create Full_Adder_tb.v

---
+++ Combinational/Full_Adder_tb.v
@@ -0,0 +1,22 @@
+module full_adder_tb;
+  wire sum,carry;
+  reg a,b,c;
+  half_adder dut(.a(a),.b(b),.c(c),.sum(sum),.carry(carry));
+
+  initial
+    begin
+      $monitor ("Time= %0t | Inputs: A=%b B=%b C=%b | Outputs: Sum=%b Carry=%b", $time, a,b,c,sum,carry);
+      $dumpfile("full_adder.vcd");
+      $dumpvars(0,full_adder_tb);
+      a=1'b0;b=1'b0;c=1'b0;
+      #10 a=1'b0;b=1'b0;c=1'b1;
+      #10 a=1'b0;b=1'b1;c=1'b0;
+      #10 a=1'b0;b=1'b1;c=1'b1;
+      #10 a=1'b1;b=1'b0;c=1'b0;
+      #10 a=1'b1;b=1'b0;c=1'b1;
+      #10 a=1'b1;b=1'b1;c=1'b0;
+      #10 a=1'b1;b=1'b1;c=1'b1;
+      #20 $finish;
+    end
+endmodule
+  
